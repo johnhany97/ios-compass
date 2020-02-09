6d71a2b88bc869b305274b000242755fb28110a9
John Ayad <johnhany97@gmail.com>
2020-02-09 21:19:57 +0000

    Initial Commit

---
+++ Compass/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Compass
+//
+//  Created by John Ayad on 09/02/2020.
+//  Copyright © 2020 John Ayad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
