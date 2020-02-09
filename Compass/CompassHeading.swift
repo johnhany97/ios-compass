8af335d166a8862a370ca11556e3fb430c165fb9
John Ayad <johnhany97@gmail.com>
2020-02-09 21:48:01 +0000

    Compass: SwiftUI

---
+++ Compass/CompassHeading.swift
@@ -0,0 +1,43 @@
+//
+//  CompassHeading.swift
+//  Compass
+//
+//  Created by John Ayad on 09/02/2020.
+//  Copyright © 2020 John Ayad. All rights reserved.
+//
+
+import Foundation
+import Combine
+import CoreLocation
+
+class CompassHeading: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var objectWillChange = PassthroughSubject<Void, Never>()
+    var degrees: Double = .zero {
+        didSet {
+            objectWillChange.send()
+        }
+    }
+    
+    private let locationManager: CLLocationManager
+    
+    override init() {
+        self.locationManager = CLLocationManager()
+        super.init()
+        
+        self.locationManager.delegate = self
+        self.setup()
+    }
+    
+    private func setup() {
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.headingAvailable() {
+            self.locationManager.startUpdatingLocation()
+            self.locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        self.degrees = -1 * newHeading.magneticHeading
+    }
+}
--- Compass/ContentView.swift
+++ Compass/ContentView.swift
@@ -9,8 +9,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var compassHeading = CompassHeading()
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Capsule().frame(width: 5, height: 50)
+            ZStack {
+                ForEach(Marker.markers(), id: \.self) { marker in
+                    CompassMarkerView(marker: marker, compassDegrees: 0.0)
+                }
+            }
+            .frame(width:  CGFloat(300), height: CGFloat(300))
+            .rotationEffect(Angle(degrees: self.compassHeading.degrees))
+            .statusBar(hidden: true)
+        }
     }
 }
 
@@ -19,3 +30,73 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Marker: Hashable {
+    let degrees: Double
+    let label: String
+
+    init(degrees: Double, label: String = "") {
+        self.degrees = degrees
+        self.label = label
+    }
+
+    func degreeText() -> String {
+        return String(format: "%.0f", self.degrees)
+    }
+
+    static func markers() -> [Marker] {
+        return [
+            Marker(degrees: 0, label: "S"),
+            Marker(degrees: 30),
+            Marker(degrees: 60),
+            Marker(degrees: 90, label: "W"),
+            Marker(degrees: 120),
+            Marker(degrees: 150),
+            Marker(degrees: 180, label: "N"),
+            Marker(degrees: 210),
+            Marker(degrees: 240),
+            Marker(degrees: 270, label: "E"),
+            Marker(degrees: 300),
+            Marker(degrees: 330)
+        ]
+    }
+}
+
+struct CompassMarkerView: View {
+    let marker: Marker
+    let compassDegrees: Double
+    
+    var body: some View {
+        VStack {
+            Text(marker.degreeText())
+                .fontWeight(.light)
+                .rotationEffect(self.textAngle())
+            Capsule()
+                .frame(width: self.capsuleWidth(), height: self.capsuleHeight())
+                .foregroundColor(self.capsuleColor())
+                .padding(.bottom, 120)
+            Text(marker.label)
+                .fontWeight(.bold)
+                .rotationEffect(self.textAngle())
+                .padding(.bottom, 80)
+        }
+        .rotationEffect(Angle(degrees: marker.degrees))
+    }
+    
+    private func capsuleWidth() -> CGFloat {
+        return self.marker.degrees == 0 ? 7 : 3
+    }
+
+    private func capsuleHeight() -> CGFloat {
+        return self.marker.degrees == 0 ? 45 : 30
+    }
+
+    private func capsuleColor() -> Color {
+        return self.marker.degrees == 0 ? .red : .gray
+    }
+
+    private func textAngle() -> Angle {
+        return Angle(degrees: -self.compassDegrees - self.marker.degrees)
+    }
+}
+
